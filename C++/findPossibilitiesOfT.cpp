3958f89ffc5c2199b8a17d4fb5cc89d2ee6235fd
Zheng Hao Tan <tanzhao@umich.edu>
2014-05-19 09:15:59 +0800

    Added explanation findPossibilitiesOfT and updated Makefile

--- C++/findPossibilitiesOfT.cpp
+++ C++/findPossibilitiesOfT.cpp
@@ -1,4 +1,5 @@
-// Zheng Hao Tan
+// Name: Zheng Hao Tan
+// Email: tanzhao@umich.edu
 // Date: 12/31/2013
 
 // Question: Given a number T, print out all the possible ways to get to T.
@@ -10,6 +11,14 @@
 // 3 + 3
 
 // Note that 3 + 2 is equal to 2 + 3, so you don't have to print both cases.
+
+// Explanation: One way that we can use to approach this question is to use a
+//				knapsack approach, along with some conditions/assumptions that
+//				we can use to help optimize our problem.
+
+// Assumptions: 1.	The numbers that we are going to take the sum of must always be
+//					more than 0. If this is false, we will get an infinite amount of
+//					possibilities.
 
 #include <iostream>
 using namespace std;
