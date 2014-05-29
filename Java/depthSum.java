e10003b7b9d91538c255468d9d9ca030fe4ab309
Zheng Hao Tan <tanzhao@umich.edu>
2014-05-29 17:10:12 +0800

    Added spacing to improve readability

--- Java/depthSum.java
+++ Java/depthSum.java
@@ -40,6 +40,6 @@ private int depthSum(IntTreeNode root, int depth)
     if (root == null)
         return 0;
     else
-        return root.data*depth + depthSum(root.left, depth+1) 
-				+ depthSum(root.right, depth+1);
+        return root.data * depth + depthSum(root.left, depth + 1) 
+				+ depthSum(root.right, depth + 1);
 }
