01fe6b85898051996c4b40f6a8741cfcfcefb783
Zheng Hao Tan <tanzhao@umich.edu>
2014-05-29 08:51:51 +0800

    Added explanation to the surroundByX question

--- C++/surroundedByX.cpp
+++ C++/surroundedByX.cpp
@@ -1,9 +1,10 @@
-// Zheng Hao Tan
+// Name: Zheng Hao Tan
+// Email: tanzhao@umich.edu
+// Date: May 29, 2014
 
-
-// Given an 2D matrix M, is filled either using X or O,
-// you need to find the region which is filled by O and surrounded by X 
-// and fill it with X. 
+// Question: Given an 2D matrix M, is filled either using X or O,
+// 			 you need to find the region which is filled by O and surrounded by X 
+// 			 and fill it with X. 
 
 // example 1: 
 
@@ -32,14 +33,19 @@
 // X X O O O 
 // O X X X X
 
+// Explanation: One of the ways that I would use to approach this problem is 
+//				dynamic programming. By doing this, we can trade memory for better
+//				runtime.
+
+// Implementation:  We can use a bottom up approach by using a 2D array (vector)
+//					to store temporary variables as we try to surround the 
+//					region filled with O.
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-
-
-
 
 
 	return 0;
